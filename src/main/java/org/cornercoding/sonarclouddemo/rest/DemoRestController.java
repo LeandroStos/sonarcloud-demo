397d42ad2197512b92fb7ca674f83828f5dfce31
L3@ndr0 B0R6ES <100321936+LeandroStos@users.noreply.github.com>
2024-06-24 22:19:46 -0300

    Update DemoRestController.java
    String teste

--- src/main/java/org/cornercoding/sonarclouddemo/rest/DemoRestController.java
+++ src/main/java/org/cornercoding/sonarclouddemo/rest/DemoRestController.java
@@ -12,6 +12,7 @@ public class DemoRestController {
 	public ResponseEntity<String> capitalize(@RequestParam("test") String text){
 		
 		String capital = text.toUpperCase();
+		String People = "Leandro Borges"
 		return ResponseEntity.ok(capital);
 		
 	}
